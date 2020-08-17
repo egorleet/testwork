cbe51e82df3f71fbabfced2eca84c512809c0a4a
egorleet <egorleet@users.noreply.github.com>
2020-08-17 07:33:28 +0400

    add

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+$('.slidertest__main').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 5000,
+  arrow: true,
+  dots: false
+});
+})
